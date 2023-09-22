b0b8ee10b26229ab05998af7adeb42abae571f59
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-09-22 20:47:02 +0300

    HW AVATAR check

--- src/main/java/ru/hogwarts/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/repository/AvatarRepository.java
@@ -1,8 +1,9 @@
 package ru.hogwarts.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.model.Avatar;
-
+@Repository
 public interface AvatarRepository extends JpaRepository<Avatar,Long> {
     Optional<Avatar> findByStudentId(Long studentId);
 
